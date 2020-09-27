d215590f077452e679491d578ed9785c946be633
Vadim Zylevich <vzy@leapwork.com>
2020-09-27 17:06:05 +0300

    Finished Factory Pattern exercise

---
+++ FactoryPattern/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace FactoryPattern
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public static PersonFactory Factory = new PersonFactory();
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}\n{nameof(Id)}: {Id}";
+        }
+    }
+
+    public class PersonFactory
+    {
+        private int _personIndex;
+
+        public Person CreatePerson(string name)
+        {
+            return new Person { Id = _personIndex++, Name = name };
+        }
+    }
+}
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace FactoryPattern
+﻿using System;
+
+namespace FactoryPattern
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var person = Person.Factory.CreatePerson("James");
+            var person2 = Person.Factory.CreatePerson("Annie");
+            var person3 = Person.Factory.CreatePerson("Bob");
+            Console.WriteLine(person);
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+            Console.ReadKey();
         }
     }
 }
