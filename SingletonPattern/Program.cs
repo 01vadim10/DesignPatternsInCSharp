73a77cbf08c75dbd23412b099fbfa5d7768788ce
Vadim Zylevich <vzy@leapwork.com>
2020-09-28 17:23:55 +0300

    Implemented Singleton exercise

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,5 +1,18 @@
-﻿namespace SingletonPattern
+﻿using System;
+
+namespace SingletonPattern
 {
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var obj1 = func.Invoke();
+            var obj2 = func.Invoke();
+
+            return obj1 == obj2;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
