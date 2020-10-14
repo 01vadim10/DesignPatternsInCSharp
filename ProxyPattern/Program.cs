3a8fd1d889900320add485fe79741205c4a4b363
Vadim Zylevich <vzy@leapwork.com>
2020-10-14 22:02:25 +0300

    Proxy pattern. Task

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ProxyPattern
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            return "drinking";
+        }
+
+        public string Drive()
+        {
+            return "driving";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "driving while drunk";
+        }
+    }
+
+    public class ResponsiblePerson
+    {
+        public ResponsiblePerson(Person person)
+        {
+            // todo
+        }
+
+        public int Age { /* todo implement property */ }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
