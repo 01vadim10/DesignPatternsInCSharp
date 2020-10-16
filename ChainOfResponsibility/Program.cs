ac4da7c3d041bdce316fb7955f305c528423ed87
Vadim Zylevich <vzy@leapwork.com>
2020-10-16 09:28:08 +0300

    Chain of Responsibility. Task

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace ChainOfResponsibility
+{
+    public abstract class Creature
+    {
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+    }
+
+    public class Goblin : Creature
+    {
+        public Goblin(Game game) { }
+    }
+
+    public class GoblinKing : Goblin
+    {
+        public GoblinKing(Game game) { }
+    }
+
+    public class Game
+    {
+        public IList<Creature> Creatures;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
