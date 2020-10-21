3c56cd97aa901ecb2de03b56e795394ba97ea639
Vadim Zylevich <vzy@leapwork.com>
2020-10-21 09:42:04 +0300

    Interpreter Pattern. Coding exercise

---
+++ InterpreterPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterPattern
+{
+    public class ExpressionProcessor
+    {
+        public Dictionary<char, int> Variables = new Dictionary<char, int>();
+
+        public int Calculate(string expression)
+        {
+            // todo
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
