852a2255f71ff0ddef75b12a703ba9223b979c34
Vadim Zylevich <vzy@leapwork.com>
2020-10-25 13:37:08 +0300

    Memento Pattern Coding Exercise. Task

---
+++ MementoPatterns/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace MementoPatterns
+{
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        // todo
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            // todo
+        }
+
+        public Memento AddToken(Token token)
+        {
+            // todo (yes, please do both overloads)
+        }
+
+        public void Revert(Memento m)
+        {
+            // todo
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
