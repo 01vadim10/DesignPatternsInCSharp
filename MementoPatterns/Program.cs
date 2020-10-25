e771f7b4812972557f83ce3d73ad11ae76c4a9c4
Vadim Zylevich <vzy@leapwork.com>
2020-10-25 14:09:44 +0300

    Completed Memento exercise

--- MementoPatterns/Program.cs
+++ MementoPatterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace MementoPatterns
 {
@@ -16,26 +17,51 @@ namespace MementoPatterns
 
     public class Memento
     {
-        // todo
+        public List<int> Values = new List<int>();
+
+        public Memento(List<Token> tokens)
+        {
+            foreach (var token in tokens)
+            {
+                Values.Add(token.Value);
+            }
+        }
     }
 
     public class TokenMachine
     {
         public List<Token> Tokens = new List<Token>();
 
+        public TokenMachine()
+        {
+        }
+
         public Memento AddToken(int value)
         {
-            // todo
+            var token = new Token(value);
+
+            return AddToken(token);
         }
 
         public Memento AddToken(Token token)
         {
-            // todo (yes, please do both overloads)
+            Tokens.Add(token);
+            var memento = new Memento(Tokens);
+
+            return memento;
         }
 
         public void Revert(Memento m)
         {
-            // todo
+            if (Tokens.Count > 0)
+            {
+                Tokens.Clear();
+                foreach (var value in m.Values)
+                {
+                    var token = new Token(value);
+                    Tokens.Add(token);
+                }
+            }
         }
     }
 
@@ -43,6 +69,55 @@ namespace MementoPatterns
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SingleTokenTest()
+        {
+            var tm = new TokenMachine();
+            var m = tm.AddToken(123);
+            tm.AddToken(456);
+            tm.Revert(m);
+            Assert.That(tm.Tokens.Count, Is.EqualTo(1));
+            Assert.That(tm.Tokens[0].Value, Is.EqualTo(123));
+        }
+
+        [Test]
+        public void TwoTokenTest()
+        {
+            var tm = new TokenMachine();
+            tm.AddToken(1);
+            var m = tm.AddToken(2);
+            tm.AddToken(3);
+            tm.Revert(m);
+            Assert.That(tm.Tokens.Count, Is.EqualTo(2));
+            Assert.That(tm.Tokens[0].Value, Is.EqualTo(1),
+                $"First token should have value 1, you got {tm.Tokens[0].Value}");
+            Assert.That(tm.Tokens[1].Value, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void FiddledTokenTest()
+        {
+            var tm = new TokenMachine();
+            Console.WriteLine("Made a token with value 111 and kept a reference");
+            var token = new Token(111);
+            Console.WriteLine("Added this token to the list");
+            tm.AddToken(token);
+            var m = tm.AddToken(222);
+            Console.WriteLine("Changed this token's value to 333 :)");
+            token.Value = 333;
+            tm.Revert(m);
+
+            Assert.That(tm.Tokens.Count, Is.EqualTo(2),
+                $"At this point, token machine should have exactly two tokens, you got {tm.Tokens.Count}");
+
+            Assert.That(tm.Tokens[0].Value, Is.EqualTo(111),
+                $"You got the token value wrong here. Hint: did you init the memento by value?");
         }
     }
 }
