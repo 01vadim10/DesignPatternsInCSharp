d9d0308ad64c8cd52cc3262a40d6573aaa6753f4
Vadim Zylevich <vzy@leapwork.com>
2020-10-27 12:31:59 +0300

    Observer Pattern. Task

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    public class Game
+    {
+        // todo
+        // remember - no fields or properties!
+    }
+
+    public class Rat : IDisposable
+    {
+        public int Attack = 1;
+
+        public Rat(Game game)
+        {
+            // todo
+        }
+
+
+        public void Dispose()
+        {
+            // todo
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
