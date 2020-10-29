16ea63d509bdb6515ed8c5b26fba05db8900f945
Vadim Zylevich <vzy@leapwork.com>
2020-10-29 20:41:19 +0300

    State Pattern. Task

---
+++ StatePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StatePattern
+{
+    public class CombinationLock
+    {
+        public CombinationLock(int[] combination)
+        {
+            // todo
+        }
+
+        // you need to be changing this on user input
+        public string Status;
+
+        public void EnterDigit(int digit)
+        {
+            // todo
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
