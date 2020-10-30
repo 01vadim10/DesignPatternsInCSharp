b442f70d2a9ece7c5b29ea89724c0410729a485b
Vadim Zylevich <vzy@leapwork.com>
2020-10-30 21:07:45 +0300

    Completed Status Pattern exercise

--- StatePattern/Program.cs
+++ StatePattern/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace StatePattern
+﻿using System;
+using NUnit.Framework;
+
+namespace StatePattern
 {
+    public enum State
+    {
+        Locked,
+        Opened,
+        Error,
+        EnteringDigits
+    }
+
     public class CombinationLock
     {
+        private int[] combination { get; }
+        private State state;
+        private int position;
+
         public CombinationLock(int[] combination)
         {
-            // todo
+            this.combination = combination;
+            Status = "LOCKED";
         }
 
         // you need to be changing this on user input
@@ -12,7 +28,39 @@
 
         public void EnterDigit(int digit)
         {
-            // todo
+            if (position == 0)
+            {
+                Status = String.Empty;
+                state = State.EnteringDigits;
+            }
+
+            if (position < combination.Length - 1 && combination[position++] == digit)
+            {
+                Status += digit.ToString();
+            }
+            else if (position == combination.Length - 1 && combination[position++] == digit)
+            {
+                state = State.Opened;
+            }
+            else
+            {
+                state = State.Error;
+            }
+
+            switch (state)
+            {
+                case State.Locked:
+                    Status = "LOCKED";
+                    break;
+                case State.Opened:
+                    Status = "OPEN";
+                    break;
+                case State.Error:
+                    Status = "ERROR";
+                    break;
+                case State.EnteringDigits:
+                    return;
+            }
         }
     }
 
@@ -20,6 +68,27 @@
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void CheckLock()
+        {
+            var cl = new CombinationLock(new []{1, 2, 3, 4, 5});
+            Assert.That(cl.Status, Is.EqualTo("LOCKED"));
+            cl.EnterDigit(1);
+            Assert.That(cl.Status, Is.EqualTo("1"));
+            cl.EnterDigit(2);
+            Assert.That(cl.Status, Is.EqualTo("12"));
+            cl.EnterDigit(2);
+            Assert.That(cl.Status, Is.EqualTo("ERROR"));
+            //cl.EnterDigit(4);
+            //Assert.That(cl.Status, Is.EqualTo("1234"));
+            //cl.EnterDigit(5);
+            //Assert.That(cl.Status, Is.EqualTo("OPEN"));
         }
     }
 }
