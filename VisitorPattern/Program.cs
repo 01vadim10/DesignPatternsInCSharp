d72bf0be70176de466b1cc6d75bc6d4990a56a9c
Vadim Zylevich <vzy@leapwork.com>
2020-10-31 23:16:31 +0300

    Completed Visitor Pattern exercise

---
+++ VisitorPattern/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace VisitorPattern
+{
+    public abstract class ExpressionVisitor
+    {
+        public virtual void Visit(Value ex){}
+
+        public virtual void Visit(AdditionExpression ex){}
+
+        public virtual void Visit(MultiplicationExpression ex){}
+    }
+
+    public abstract class Expression
+    {
+        public abstract void Accept(ExpressionVisitor ev);
+    }
+
+    public class Value : Expression
+    {
+        public readonly int TheValue;
+
+        public Value(int value)
+        {
+            TheValue = value;
+        }
+
+        public override void Accept(ExpressionVisitor ev)
+        {
+            ev.Visit(this);
+        }
+    }
+
+    public class AdditionExpression : Expression
+    {
+        public readonly Expression LHS, RHS;
+
+        public AdditionExpression(Expression lhs, Expression rhs)
+        {
+            LHS = lhs;
+            RHS = rhs;
+        }
+
+        // todo
+        public override void Accept(ExpressionVisitor ev)
+        {
+            ev.Visit(this);
+        }
+    }
+
+    public class MultiplicationExpression : Expression
+    {
+        public readonly Expression LHS, RHS;
+
+        public MultiplicationExpression(Expression lhs, Expression rhs)
+        {
+            LHS = lhs;
+            RHS = rhs;
+        }
+
+        // todo
+        public override void Accept(ExpressionVisitor ev)
+        {
+            ev.Visit(this);
+        }
+    }
+
+    public class ExpressionPrinter : ExpressionVisitor
+    {
+        private StringBuilder sb = new StringBuilder();
+        public override void Visit(Value value)
+        {
+            sb.Append(value.TheValue);
+        }
+
+        public override void Visit(AdditionExpression ae)
+        {
+            sb.Append("(");
+            ae.LHS.Accept(this);
+            sb.Append("+");
+            ae.RHS.Accept(this);
+            sb.Append(")");
+        }
+
+        public override void Visit(MultiplicationExpression me)
+        {
+            me.LHS.Accept(this);
+            sb.Append("*");
+            me.RHS.Accept(this);
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void SimpleAddition()
+        {
+            var simple = new AdditionExpression(new Value(2), new Value(3));
+            var ep = new ExpressionPrinter();
+            ep.Visit(simple);
+            Assert.That(ep.ToString(), Is.EqualTo("(2+3)"));
+        }
+    }
+}
